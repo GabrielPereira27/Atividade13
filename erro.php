516a66796c266d9ba8ca3abf5248673382888eb8
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-11-06 11:20:34 +0000

    Update erro.php, footer.php, and 4 more files...

---
+++ erro.php
@@ -0,0 +1,4 @@
+<?php 
+	echo "ERRO!<br>";
+	echo "Introduziu a <B>login</B> ou <B>Password</B> errados.<br>";
+?>
---
+++ main.php
@@ -0,0 +1,4 @@
+<?php 
+	include("header.php");
+	echo "Aqui ficará a parte do <B>conteúdo</B> (Esta será a parte variável do nosso site)<br>";
+?>
